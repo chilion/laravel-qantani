b8af1491a4a8a5ef16e8cebc658ae8739bef3741
Chilion Snoek <chilion@josephdesign.nl>
2015-10-02 10:17:14 +0200

    Updated version

--- src/Qantani.php
+++ src/Qantani.php
@@ -6,8 +6,8 @@ use \SimpleXMLElement;
 
 class Qantani
 {
-     public function __construct() {
-        return $this;
+    public function __construct() {
+
     }
 
 
@@ -18,13 +18,14 @@ class Qantani
         $data = self::executeRequest('IDEAL.GETBANKS', $parameters, array('Response', 'Banks', 'Bank'));
         $res = [];
 
+        dd($data);
         foreach($data as $item){
             $res[] = [
                 'Name'  => $item['Name']['value'],
                 'Id'    => $item['Id']['value']
             ];
         }
-        return $res;
+        dd($res);
     }
 
 
@@ -46,20 +47,26 @@ class Qantani
             )
         );
 
+        $xml = self::_encodeXML($data);
+
+
         $ch = curl_init(self::$apiUrl);
         curl_setopt($ch, CURLOPT_VERBOSE, true);
         curl_setopt($ch, CURLOPT_URL, self::$apiUrl);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt ($ch, CURLOPT_POST, true);
+        curl_setopt ($ch, CURLOPT_POSTFIELDS, 'data=' . urlencode($xml));
         curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
 
         $returndata = curl_exec($ch);
         $returndata = trim($returndata);
 
+        return self::_decodexml($returndata);
+
 
     }
 
-    private function _checksum($parameters){
+    private static function _checksum($parameters){
         ksort($parameters);
         $checksum = '';
         foreach($parameters as $k=>$v){
@@ -68,25 +75,163 @@ class Qantani
 
         $lines = array();
 
+        return sha1($checksum . implode('', $lines) . config("qantani.merchant_secret"));
+    }
 
-        // insert products
-        if (count($this->_products)){
-            foreach($this->_products as $index=>$product){
-                $lines[] .= $product['Amount'] . $product['Currency'] . $product['Description'] . $product['ID'] . $product['Price'] . $product['Vat'];
+    private static function _encodeXML($data, $level=0){
+        $xml = ($level == 0) ? '<?xml version="1.0" encoding="UTF-8"?' .'>' . "\n" : '';
+
+        foreach($data as $k=>$v){
+            $xml .= str_repeat("\t", $level);
+            if (preg_match('/^(.+)\.(\d+)$/', $k, $matches))
+                $k = $matches[1];
+            $xml .= '<' . $k;
+            $children = array();
+            if (is_array($v)){
+                foreach($v as $k2=>$v2){
+                    if (substr($k2, 0, 5) == 'attr:'){
+                        $xml .= ' ' . substr($k2,5) . '="' . $v2 . '"';
+                        unset($v[$k2]);
+                    }
+                }
+            }
+            if (is_array($v) && count($v)){
+                $xml .= '>' . "\n";
+                $xml .= self::_encodeXML($v, $level+1);
+                $xml .= str_repeat("\t", $level) . '</' . $k . '>' . "\n";
+            }
+            else{
+                if ($v){
+                    $xml .= '>' . htmlentities($v) . '</' . $k . '>' . "\n";
+                }
+                else{
+                    $xml .= ' />' . "\n";
+                }
+            }
+
+        }
+        return $xml;
+    }
+
+    private static function _decodeXML($contents){
+        $get_attributes = false;
+
+        if(!$contents) return array();
+        $priority = '';
+
+        if(!function_exists('xml_parser_create')) {
+            return array();
+        }
+
+        $parser = xml_parser_create('');
+        xml_parser_set_option($parser, XML_OPTION_TARGET_ENCODING, "UTF-8");
+        xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
+        xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, 1);
+        xml_parse_into_struct($parser, trim($contents), $xml_values);
+        xml_parser_free($parser);
+        if(!$xml_values) return;
+
+
+        $xml_array = array();
+        $parents = array();
+        $opened_tags = array();
+        $arr = array();
+
+        $current = &$xml_array;
+
+        $repeated_tag_index = array();
+        foreach($xml_values as $data) {
+            unset($attributes,$value);
+
+
+            extract($data);
+
+            $result = array();
+            $attributes_data = array();
+
+            if(isset($value)) {
+                if($priority == 'tag') $result = $value;
+                else $result['value'] = $value;
+            }
+
+
+            if(isset($attributes) and $get_attributes) {
+                foreach($attributes as $attr => $val) {
+                    if($priority == 'tag') $attributes_data[$attr] = $val;
+                    else $result['attr'][$attr] = $val;
+                }
+            }
+
+
+            if($type == "open") {
+                $parent[$level-1] = &$current;
+                if(!is_array($current) or (!in_array($tag, array_keys($current)))) {
+                    $current[$tag] = $result;
+                    if($attributes_data) $current[$tag. '_attr'] = $attributes_data;
+                    $repeated_tag_index[$tag.'_'.$level] = 1;
+
+                    $current = &$current[$tag];
+
+                } else {
+                    if(isset($current[$tag][0])) {
+                        $current[$tag][$repeated_tag_index[$tag.'_'.$level]] = $result;
+                        $repeated_tag_index[$tag.'_'.$level]++;
+                    } else {
+                        $current[$tag] = array($current[$tag],$result);
+                        $repeated_tag_index[$tag.'_'.$level] = 2;
+
+                        if(isset($current[$tag.'_attr'])) {
+                            $current[$tag]['0_attr'] = $current[$tag.'_attr'];
+                            unset($current[$tag.'_attr']);
+                        }
+
+                    }
+                    $last_item_index = $repeated_tag_index[$tag.'_'.$level]-1;
+                    $current = &$current[$tag][$last_item_index];
+                }
+
+            } elseif($type == "complete") {
+
+                if(!isset($current[$tag])) {
+                    $current[$tag] = $result;
+                    $repeated_tag_index[$tag.'_'.$level] = 1;
+                    if($priority == 'tag' and $attributes_data) $current[$tag. '_attr'] = $attributes_data;
+
+                } else {
+                    if(isset($current[$tag][0]) and is_array($current[$tag])) {
+
+                        $current[$tag][$repeated_tag_index[$tag.'_'.$level]] = $result;
+
+                        if($priority == 'tag' and $get_attributes and $attributes_data) {
+                            $current[$tag][$repeated_tag_index[$tag.'_'.$level] . '_attr'] = $attributes_data;
+                        }
+                        $repeated_tag_index[$tag.'_'.$level]++;
+
+                    } else {
+                        $current[$tag] = array($current[$tag],$result);
+                        $repeated_tag_index[$tag.'_'.$level] = 1;
+                        if($priority == 'tag' and $get_attributes) {
+                            if(isset($current[$tag.'_attr'])) {
+
+                                $current[$tag]['0_attr'] = $current[$tag.'_attr'];
+                                unset($current[$tag.'_attr']);
+                            }
+
+                            if($attributes_data) {
+                                $current[$tag][$repeated_tag_index[$tag.'_'.$level] . '_attr'] = $attributes_data;
+                            }
+                        }
+                        $repeated_tag_index[$tag.'_'.$level]++;
+                    }
+                }
+
+            } elseif($type == 'close') {
+                $current = &$parent[$level-1];
             }
         }
 
-        // insert customer
-        if (count($this->_customer)){
-            ksort($this->_customer);
-            $line = '';
-            foreach($this->_customer as $v){
-                $line .= $v;
-            }
-            $lines[] = $line;
-        }
+        return($xml_array);
 
-        return sha1($checksum . implode('', $lines) . config("qantani.merchant_key"));
     }
 
 }
